ef26002b76f5d9513cb36c73d68e587ef841e035
dudinhaa0 <maria.e.dias22@aluno.senai.br>
2026-04-10 10:04:28 -0300

    Add files via upload

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../data/database');
+const supabase = require('../data/database');
 
 
 router.get('/', async (req, res, next) => {
